ccb69cf8ab35482b6ae0cd9e6c56f676416a1d8c
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-09 09:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// This is the boilerplate code given for you
+// You can modify this code
+// Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
