b57fe0f17606eccc44251d4a98c0c4f62155e906
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-09 09:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -50,13 +50,12 @@ function renderCart(cart) {
 function addToCart(product) {
   let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
-  // Check if the product already exists in the cart
-  const existingProduct = cart.find(item => item.id === product.id);
-  if (!existingProduct) {
-    cart.push(product);
-    saveCart(cart);
-    renderCart(cart);
-  }
+  // Prevent duplicate product entries
+  cart = cart.filter(item => item.id !== product.id);
+  cart.push(product);
+
+  saveCart(cart);
+  renderCart(cart);
 }
 
 // Clear cart
